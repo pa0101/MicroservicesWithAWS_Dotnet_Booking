c592df0fee63a66d3de665da76411e105a4ddcd6
pa0101 <paolodipietro.dev@koderhythm.com>
2024-07-04 13:36:16 +1000

    Small clean up

--- Booking.Command/Models/BookingDto.cs
+++ Booking.Command/Models/BookingDto.cs
@@ -9,15 +9,13 @@ public class BookingDto
     public string? HotelId { get; set; }
     public string? CheckinDate { get; set; }
     public string? CheckoutDate { get; set; }
-
-    [DynamoDBHashKey("UserId")] public string? UserId { get; set; }
-
-    [DynamoDBRangeKey("Id")] public string? Id { get; set; }
-
+    [DynamoDBHashKey("UserId")]
+    public string? UserId { get; set; }
+    [DynamoDBRangeKey("Id")]
+    public string? Id { get; set; }
     public string? GivenName { get; set; }
     public string? FamilyName { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Email { get; set; }
-
     public BookingStatus Status { get; set; }
 }
